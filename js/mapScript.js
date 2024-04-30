3644e60b6bab3386eb461cbd52565fef85b1a025
SmoldersKasper <kasper.smolders@gmail.com>
2024-04-30 12:13:21 +0200

    Recepten en contact

---
+++ js/mapScript.js
@@ -0,0 +1,24 @@
+/* In dit script plaats je de code om de kaart in de id apMap te tekenen, zodat de AP-Hogeschool met adres Ellermanstraat 33 gecentreerd staat.  
+* Gebruik hiervoor de documentatie op https://leafletjs.com/ 
+* Verander in de code online alle "var" in "let".
+*/
+let map = L.map('apMap').setView([51.23009, 4.41616], 17);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+let koffieIcon = L.icon({
+    iconUrl: "assets/coffee-icon.png",
+    shadowUrl: "assets/coffee-icon-shadow.png",
+
+    iconSize:     [38, 50],
+    shadowSize:   [38, 50],
+    iconAnchor:   [0, 50],
+    shadowAnchor: [0, 50],
+    popupAnchor:  [-3, -76]
+})
+
+
+let marker = L.marker([51.23009, 4.41616],{icon: koffieIcon}).addTo(map);
